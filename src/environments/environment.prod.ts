423bfbc51bd0e4ced219977a29950097d9d54f8d
Cristi Muresan <cristimuresan1011@gmail.com>
2020-04-06 17:59:31 +0300

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCEHETkOefqTi7lWY11FnaMkAIvcYQPaPE",
+    authDomain: "shshop-2dd1a.firebaseapp.com",
+    databaseURL: "https://shshop-2dd1a.firebaseio.com",
+    projectId: "shshop-2dd1a",
+    storageBucket: "shshop-2dd1a.appspot.com",
+    messagingSenderId: "562637536270",
+    appId: "1:562637536270:web:c8b31fe898a46a98fa905f",
+    measurementId: "G-YQM2CVM4D1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCEHETkOefqTi7lWY11FnaMkAIvcYQPaPE",
+    authDomain: "shshop-2dd1a.firebaseapp.com",
+    databaseURL: "https://shshop-2dd1a.firebaseio.com",
+    projectId: "shshop-2dd1a",
+    storageBucket: "shshop-2dd1a.appspot.com",
+    messagingSenderId: "562637536270",
+    appId: "1:562637536270:web:c8b31fe898a46a98fa905f",
+    measurementId: "G-YQM2CVM4D1"
+  }
 };
 
 /*
